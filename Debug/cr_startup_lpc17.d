98eb7bdc0778aaab1aca623aecb72fb152146a95
Dante <dantesangiovanni@gmail.com>
2017-10-25 00:50:56 -0300

    Primer Commit de Dante

---
+++ Debug/cr_startup_lpc17.d
@@ -0,0 +1,4 @@
+cr_startup_lpc17.d cr_startup_lpc17.o: ../cr_startup_lpc17.c \
+ /home/andresdido/workspace/CMSISv1p30_LPC17xx/inc/system_LPC17xx.h
+
+/home/andresdido/workspace/CMSISv1p30_LPC17xx/inc/system_LPC17xx.h:
---
+++ Debug/main.d
@@ -0,0 +1,37 @@
+main.d main.o: ../main.c \
+ /home/andresdido/workspace/FreeRTOS_Library/include/FreeRTOS.h \
+ /home/andresdido/workspace/FreeRTOS_Library/include/projdefs.h \
+ /home/andresdido/workspace/FreeRTOS_Library/include/FreeRTOSConfig.h \
+ /home/andresdido/workspace/CMSISv1p30_LPC17xx/inc/LPC17xx.h \
+ /home/andresdido/workspace/CMSISv1p30_LPC17xx/inc/core_cm3.h \
+ /home/andresdido/workspace/CMSISv1p30_LPC17xx/inc/system_LPC17xx.h \
+ /home/andresdido/workspace/FreeRTOS_Library/include/portable.h \
+ /home/andresdido/workspace/FreeRTOS_Library/portable/portmacro.h \
+ /home/andresdido/workspace/FreeRTOS_Library/include/mpu_wrappers.h \
+ /home/andresdido/workspace/FreeRTOS_Library/include/task.h \
+ /home/andresdido/workspace/FreeRTOS_Library/include/list.h \
+ /home/andresdido/workspace/FreeRTOS_Library/demo_code/basic_io.h
+
+/home/andresdido/workspace/FreeRTOS_Library/include/FreeRTOS.h:
+
+/home/andresdido/workspace/FreeRTOS_Library/include/projdefs.h:
+
+/home/andresdido/workspace/FreeRTOS_Library/include/FreeRTOSConfig.h:
+
+/home/andresdido/workspace/CMSISv1p30_LPC17xx/inc/LPC17xx.h:
+
+/home/andresdido/workspace/CMSISv1p30_LPC17xx/inc/core_cm3.h:
+
+/home/andresdido/workspace/CMSISv1p30_LPC17xx/inc/system_LPC17xx.h:
+
+/home/andresdido/workspace/FreeRTOS_Library/include/portable.h:
+
+/home/andresdido/workspace/FreeRTOS_Library/portable/portmacro.h:
+
+/home/andresdido/workspace/FreeRTOS_Library/include/mpu_wrappers.h:
+
+/home/andresdido/workspace/FreeRTOS_Library/include/task.h:
+
+/home/andresdido/workspace/FreeRTOS_Library/include/list.h:
+
+/home/andresdido/workspace/FreeRTOS_Library/demo_code/basic_io.h:
